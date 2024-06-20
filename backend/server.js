c88c912338c6dbb74a6c573fedaa2efd36879cc9
Mattey2906 <matthias.pfrendl@stud.fh-campuswien.ac.at>
2024-06-20 17:36:49 +0200

    node.js + express, react installiert

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Statische Dateien aus dem build-Verzeichnis der React-Anwendung bedienen
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
