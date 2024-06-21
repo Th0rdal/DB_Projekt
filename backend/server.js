b371de5a836bedc6eebc828401552e55a3e601f3
Mattey2906 <matthias.pfrendl@stud.fh-campuswien.ac.at>
2024-06-21 21:37:40 +0200

    /register, /get_BLZ

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const { generateUniqueIdentification } = require('./utils');
+const {generateUniqueIdentification} = require('./utils');
+
+const db = require('../database/db');
 
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Statische Dateien aus dem build-Verzeichnis der React-Anwendung bedienen
@@ -17,50 +18,117 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 
-app.post('/register', (req, res) => {
-    const { SVNR, firstName, lastName, phoneNr1, phoneNr2, ZIP, street, city, streetNr, accountBalance, accountNr, BLZ} = req.body;
+app.get('/get_BLZ', async (req, res) => {
+    try {
+        const getAllBLZ = () => {
+            return new Promise((resolve, reject) => {
+                const query = 'SELECT BLZ FROM BankName';
+                db.all(query, [], (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(rows);
+                    }
+                });
+            });
+        };
+
+        const blzList = await getAllBLZ();
+        res.status(200).json(blzList);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.post('/register', async (req, res) => {
+    const {
+        SVNR,
+        firstName,
+        lastName,
+        phoneNr1,
+        phoneNr2,
+        ZIP,
+        street,
+        city,
+        streetNr,
+        accountBalance,
+        accountNr,
+        BLZ
+    } = req.body;
 
     if (!SVNR || !firstName || !lastName || !phoneNr1 || !ZIP || !street || !city || !streetNr || !accountBalance || !accountNr || !BLZ) {
         return res.status(400).json({ error: 'Missing required fields' });
     }
 
-    //check if BLZ exists
-    const checkBLZQuery = 'SELECT BankName FROM BankName Bank WHERE BLZ = ?';
-    db.get(checkBLZQuery, [BLZ], (err, row) => {
-        if (err) {
-            return res.status(500).json({error: err.message});
-        }
-        if (!row) {
-            return res.status(400).json({error: 'BLZ does not exist in the BankName table'});
-        }
-
-
-        //generate Unique Identification for Instructor (use as SessionID later)
-
-        //get hiringDate
-        generateUniqueIdentification((err, identification) => {
-            if (err) {
-                return res.status(500).json({ error: err.message });
-            }
-
-            const employeeQuery = `INSERT INTO Employee (Identification, SVNR, AccountBalance, AccountNr, BLZ)
-                   VALUES (?, ?, ?, ?, ?)`;
-
-
-            db.run(employeeQuery, [identification, SVNR, accountBalance])
-
-
-            const personQuery = `INSERT INTO Person (SVNR, firstName, lastName, phoneNr1, phoneNr2, ZIP, street, city, streetNr)
-                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-
-            db.run(personQuery, [SVNR, firstName, lastName, phoneNr1, phoneNr2 || null, ZIP, street, city, streetNr], function(err) {
-                if (err) {
-                    return res.status(500).json({error: err.message});
-                }
-                res.status(200).json({message: 'User registered successfully', data: req.body});
+    try {
+        const checkBLZ = (BLZ) => {
+            return new Promise((resolve, reject) => {
+                const query = 'SELECT BankName FROM BankName WHERE BLZ = ?';
+                db.get(query, [BLZ], (err, row) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(row);
+                    }
+                });
             });
-        });
-    });
+        };
+
+        const insertPerson = (SVNR, firstName, lastName, phoneNr1, phoneNr2, ZIP, street, city, streetNr) => {
+            return new Promise((resolve, reject) => {
+                const query = `INSERT INTO Person (SVNR, firstName, lastName, phoneNr1, phoneNr2, ZIP, street, city, streetNr) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+                db.run(query, [SVNR, firstName, lastName, phoneNr1, phoneNr2 || null, ZIP, street, city, streetNr], function (err) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        };
+
+        const insertEmployee = (SVNR, accountBalance, accountNr, BLZ) => {
+            return new Promise((resolve, reject) => {
+                const query = `INSERT INTO Employee (SVNR, AccountBalance, AccountNr, BLZ) VALUES (?, ?, ?, ?)`;
+                db.run(query, [SVNR, accountBalance, accountNr, BLZ], function (err) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        };
+
+        const insertInstructor = (identification, currentDate, SVNR) => {
+            return new Promise((resolve, reject) => {
+                const query = `INSERT INTO Instructor (Identification, HiringDate, SVNR) VALUES (?, ?, ?)`;
+                db.run(query, [identification, currentDate, SVNR], function (err) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        };
+
+        const BLZRow = await checkBLZ(BLZ);
+        if (!BLZRow) {
+            return res.status(400).json({ error: 'BLZ does not exist in the BankName table' });
+        }
+
+        const identification = await generateUniqueIdentification();
+        const currentDate = new Date();
+
+        await insertPerson(SVNR, firstName, lastName, phoneNr1, phoneNr2, ZIP, street, city, streetNr);
+        await insertEmployee(SVNR, accountBalance, accountNr, BLZ);
+        await insertInstructor(identification, currentDate, SVNR);
+
+        res.status(200).json({ message: 'User registered successfully', data: req.body });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 app.listen(port, () => {
---
+++ database/db.js
@@ -0,0 +1,7 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const dbPath = path.resolve(__dirname, 'database.db');
+const db = new sqlite3.Database(dbPath);
+
+module.exports = db;
