25eb54f6dfd38bb65d54179200843248c17ab0c8
Mattey2906 <matthias.pfrendl@stud.fh-campuswien.ac.at>
2024-06-21 21:37:51 +0200

    /register, /get_BLZ

--- backend/utils.js
+++ backend/utils.js
@@ -1,20 +1,19 @@
 const db = require('../database/db');
 
-function generateUniqueIdentification(callback) {
-    const identification = Math.floor(1000000000 + Math.random() * 9000000000).toString(); // 10-stellige Zahl
+function generateUniqueIdentification() {
+    return new Promise((resolve, reject) => {
+        const identification = Math.floor(1000000000 + Math.random() * 9000000000).toString(); // 10-stellige Zahl
 
-    const checkIdentificationQuery = 'SELECT Identification FROM Instructor WHERE Identification = ?';
-    db.get(checkIdentificationQuery, [identification], (err, row) => {
-        if (err) {
-            return callback(err, null);
-        }
-        if (row) {
-            // Wenn die Identifikationsnummer bereits existiert, generiere eine neue
-            generateUniqueIdentification(callback);
-        } else {
-            // Wenn die Identifikationsnummer einzigartig ist, gib sie zurück
-            callback(null, identification);
-        }
+        const checkIdentificationQuery = 'SELECT Identification FROM Instructor WHERE Identification = ?';
+        db.get(checkIdentificationQuery, [identification], (err, row) => {
+            if (err) {
+                reject(err);
+            } else if (row) {
+                generateUniqueIdentification().then(resolve).catch(reject);
+            } else {
+                resolve(identification);
+            }
+        });
     });
 }
 
