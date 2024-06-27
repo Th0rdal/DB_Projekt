d58bcfadaf7c5792fdbf4925f7877c8c291a59f2
Mattey2906 <matthias.pfrendl@stud.fh-campuswien.ac.at>
2024-06-27 13:07:24 +0200

    additional quick fix

--- backend/create.js
+++ backend/create.js
@@ -315,12 +315,16 @@ const getUsersSeminars = async (SVNR) => {
       SELECT *
       FROM SEMINAR S
       JOIN ADDRESS A ON A.addressID = S.addressID
-      JOIN COURSE C ON C.courseName = S.courseName
-      JOIN INSTRUCTOR I ON I.SVNR = S.instructor
+      JOIN (
+        SELECT SVNR, identification
+        FROM INSTRUCTOR
+      ) as I
+      ON I.SVNR = S.instructor
       JOIN (
         SELECT SVNR, firstname, lastname
         FROM PERSON
-      ) ON P.SVNR = I.SVNR
+      ) as P
+      ON P.SVNR = I.SVNR
       WHERE S.instructor = ?;
     `;
     const rows = await DBAbstraction.all(query, [SVNR]);
